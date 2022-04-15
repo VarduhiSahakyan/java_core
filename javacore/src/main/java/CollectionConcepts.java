518f5b0ca2cf18e0e1b90d325804552aa1ae0c02
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-04-15 17:31:22 +0400

    first commit

---
+++ javacore/src/main/java/CollectionConcepts.java
@@ -0,0 +1,9 @@
+public class CollectionConcepts {
+
+    public static void main(String[] args) {
+        Product door = new Product("door", 37);
+        Product window = new Product("window", 19);
+        Product floorPanel = new Product("floor panel", 15);
+
+    }
+}
---
+++ javacore/src/main/java/Product.java
@@ -0,0 +1,27 @@
+public class Product {
+
+    public String name;
+
+    public int weight;
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
